1270068aa6cde870d2614c057498ae4a5f6f9f56
dlsyj <dlsyj@users.noreply.github.com>
2016-06-08 13:30:26 -0400

    [62. Unique Paths][Accepted]DP solution, with 1 level array

--- 62-Unique-Paths/firstRound.java
+++ 62-Unique-Paths/firstRound.java
@@ -1,14 +1,16 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        return helper(1,1,m,n);
-    }
-    
-    private int helper(int row, int col, int m, int n)
-    {
-        if(row==m && col==n)
-            return 1;
-        if(row>m || col>n)
+        if(m<=0 || n<=0)
             return 0;
-        return helper(row+1,col,m,n)+helper(row,col+1,m,n);
+        int[] ans = new int[n];
+        ans[0] = 1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+               ans[j] += ans[j-1];
+            }
+        }
+        return ans[n-1];
     }
 }
