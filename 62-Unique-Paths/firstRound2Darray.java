b966d77b49623200a60425510254ba358e89ad43
dlsyj <dlsyj@users.noreply.github.com>
2016-06-08 14:00:52 -0400

    [62. Unique Paths][Accepted]first round with 1D array

--- 62-Unique-Paths/firstRound2Darray.java
+++ 62-Unique-Paths/firstRound2Darray.java
@@ -1,17 +1,21 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        Integer[][] map = new Integer[m][n];
-        for(int i = 0; i<m;i++){
-            map[i][0] = 1;
-        }
-        for(int j= 0;j<n;j++){
-            map[0][j]=1;
-        }
-        for(int i = 1;i<m;i++){
-            for(int j = 1;j<n;j++){
-                map[i][j] = map[i-1][j]+map[i][j-1];
+            if (m == 0 || n == 0) {
+                return 0;
             }
-        }
-        return map[m-1][n-1];
+            if (m == 1 || n == 1) {
+                return 1;
+            }
+    
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++) {
+                dp[i] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+            return dp[n - 1];
     }
 }
