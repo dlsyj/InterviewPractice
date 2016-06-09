69b0d7a8c49320403e2c5ca4843e4e154e0274c0
dlsyj <dlsyj@users.noreply.github.com>
2016-06-08 20:59:28 -0400

    [63. Unique Paths II][Runtime Error] First Round: 1D array solution

---
+++ 63-Unique-Paths-II/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0){
+            return 0;
+        }
+        int[] ans = new int[obstacleGrid.length];
+        ans[0] = 1;
+        for (int i=0; i < obstacleGrid.length; i++){
+            for (int j=0; j < obstacleGrid[0].length; j++){
+                if (obstacleGrid[i][j] == 1){
+                    ans[j] = 0;
+                } else {
+                    if (j > 0){
+                        ans[j] += ans[j-1];
+                    }
+                }
+            }
+        }
+        return ans[obstacleGrid.length-1];
+    }
+}
