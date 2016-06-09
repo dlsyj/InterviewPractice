8b2276e499e75abf2e9d434eb5eb406fda5602f2
dlsyj <dlsyj@users.noreply.github.com>
2016-06-09 12:57:12 -0400

    [75. Sort Colors][Accepted]quick review after lunch

---
+++ 75-Sort-Colors/quickwrite.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0){
+    		return;
+    	}
+    	// Counting Sort
+    	int[] helper = new int[3];
+    	int[] ans = new int[nums.length];
+    	for (int i=0; i < nums.length; i++){
+    		helper[nums[i]]++;
+    	}
+    	helper[1] += helper[0];
+    	helper[2] += helper[1];
+    	for (int i=0; i < nums.length; i++){
+    		ans[helper[nums[i]]-1] = nums[i];
+    		helper[nums[i]]--;
+    	}
+    	for (int i=0; i < nums.length; i++){
+    		nums[i] = ans[i];
+    	}
+    }
+}
