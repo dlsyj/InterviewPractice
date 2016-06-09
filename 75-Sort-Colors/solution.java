ee80df43e2eb87fdfa915449e0bb855aa8d3bfd3
dlsyj <dlsyj@users.noreply.github.com>
2016-06-08 22:48:31 -0400

    [75. Sort Colors][Accepted] First Round: counting sort

---
+++ 75-Sort-Colors/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return;
+        }
+        int[] ans = new int[nums.length];
+        int[] helper= new int[3];
+        for (int i=0; i < nums.length; i++){
+            helper[nums[i]]++;
+        }
+        helper[1] += helper[0];
+        helper[2] += helper[1];
+        for (int i=nums.length-1; i>=0; i--){
+            ans[helper[nums[i]]-1] = nums[i];
+            helper[nums[i]]--;
+        }
+        for (int i=0; i<nums.length; i++){
+            nums[i] = ans[i];
+        }
+    }
+}
