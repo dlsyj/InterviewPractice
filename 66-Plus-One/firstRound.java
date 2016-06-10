65e980d1a6ebd2a9323a289e49c96cc5793b419a
dlsyj <dlsyj@users.noreply.github.com>
2016-06-09 23:22:17 -0400

    [66. Plus One][Accepted] First Round:

--- 66-Plus-One/firstRound.java
+++ 66-Plus-One/firstRound.java
@@ -4,7 +4,7 @@ public class Solution {
             return digits;
         }
         int carry = 1;
-        for (int i = digits.length-1; i >= 0; i++){
+        for (int i = digits.length-1; i >= 0; i--){
             int digit = (digits[i] + carry)%10;
             carry = (digits[i] + carry)/10;
             digits[i] = digit;
