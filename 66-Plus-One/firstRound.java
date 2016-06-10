83fbe8e68caa197998d8e39309d8fb903873e334
dlsyj <dlsyj@users.noreply.github.com>
2016-06-09 23:22:03 -0400

    [66. Plus One][Runtime Error] First Round:

---
+++ 66-Plus-One/firstRound.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        if (digits == null || digits.length == 0){
+            return digits;
+        }
+        int carry = 1;
+        for (int i = digits.length-1; i >= 0; i++){
+            int digit = (digits[i] + carry)%10;
+            carry = (digits[i] + carry)/10;
+            digits[i] = digit;
+            if (carry == 0){
+                return digits;
+            }
+        }
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
