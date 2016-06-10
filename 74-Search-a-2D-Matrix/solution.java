8814bca374a1155476dbc7476438ed71b1cefd19
dlsyj <dlsyj@users.noreply.github.com>
2016-06-10 14:58:09 -0400

    [74. Search a 2D Matrix][Compile Error]first round

---
+++ 74-Search-a-2D-Matrix/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+    	if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+    		return false;
+    	}
+    	int m = matrix.length, n = matrix[0].length;
+    	int low = 0, high = m*n-1;
+    	while (low ! high){
+    		int mid = low + (high - low)/2;
+    		if (matrix[mid/n][mid%n] < target){
+    			low = mid+1;
+    		} else {
+    			high = mid;
+    		}
+    	}
+    	return matrix[mid/n][mid%n] == target;     
+    }
+}
