9025a4385a68d928624fcef7c76fd93db45ba05d
dlsyj <dlsyj@users.noreply.github.com>
2016-06-11 17:28:51 -0400

    [26. Remove Duplicates from Sorted Array][Accepted] First Round:

---
+++ 26-Remove-Duplicates-from-Sorted-Array/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0 ){
+            return 0;
+        }
+        int uniqueID = 1;
+        for (int i = 1; i< nums.length; i++){
+            if (nums[i] != nums[i-1]){
+                nums[uniqueID++] = nums[i];
+            }
+        }
+        return uniqueID;
+    }
+}
