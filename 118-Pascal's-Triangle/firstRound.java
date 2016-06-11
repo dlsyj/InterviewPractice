f9f4650e8737bec7e1bdab10814e8683f9a5386f
dlsyj <dlsyj@users.noreply.github.com>
2016-06-11 14:29:20 -0400

    [118. Pascal's Triangle][Accepted] First Round: use one arraylist

---
+++ 118-Pascal's-Triangle/firstRound.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> eachRow = new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++){
+            eachRow.add(0,1);
+            for (int j = 1; j < eachRow.size()-1; j++){
+                eachRow.set(j,eachRow.get(j)+eachRow.get(j+1));
+            }
+            ans.add(new ArrayList<Integer>(eachRow));
+        }
+        return ans;
+    }
+}
