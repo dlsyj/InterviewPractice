93d8901cbbafd0f8288a209378e218c6700bcbcf
dlsyj <dlsyj@users.noreply.github.com>
2016-06-15 22:06:20 -0400

    [73. Set Matrix Zeroes][Accepted] First Round:

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -7,10 +7,9 @@ public class Solution {
         boolean fr = false, fc = false;
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                if (matrix[0][j] == 0) fr = true;
-                
-                if (matrix[i][0] == 0) fc = true;
                 if (matrix[i][j] == 0){
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
                 }
@@ -19,7 +18,7 @@ public class Solution {
         
         for (int i = 1; i < m; i++){
             for (int j = 1; j < n; j++){
-                if (matrix[0][j] == 0 || matrix[i][0]) matrix[i][j] = 0;
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
             }
         }
         
@@ -30,6 +29,5 @@ public class Solution {
         if (fc){
             for (int i = 0; i < m; i++) matrix[i][0] = 0;
         }
-        return;
     }
 }
