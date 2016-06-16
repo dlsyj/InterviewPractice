caa4c8250c3fb8c0d2101df569b2fd1f88b0dc61
dlsyj <dlsyj@users.noreply.github.com>
2016-06-15 23:22:56 -0400

    [162. Find Peak Element][Compile Error] First Round: Rercusive  solution

--- 162-Find-Peak-Element/firstRound.java
+++ 162-Find-Peak-Element/firstRound.java
@@ -1,14 +1,17 @@
 public class Solution {
     public int findPeakElement(int[] nums) {
-       int left = 0, right = nums.length-1;
-       while (left < right){
-           int mid = left + (right - left)/2;
-           if (nums[mid] < nums[mid+1]){
-               left = mid+1;
-           } else {
-               right = mid;
-           }
-       }
-       return left;
+       return helper(nums, 0, nums.length -1);
+    }
+    
+    public void helper(int[] nums, int left, int right){
+        if (left ==  rihgt){
+            return left;
+        }
+        int mid = left + (rihgt - left);
+        if (nums[mid] < nums[mid+1]){
+            left = mid+1;
+        } else {
+            right = mid;
+        }
     }
 }
