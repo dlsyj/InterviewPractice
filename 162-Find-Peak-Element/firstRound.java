033e7283b22b8b0796f514aeb4cd5a5ccf0b6fca
dlsyj <dlsyj@users.noreply.github.com>
2016-06-15 23:42:00 -0400

    [162. Find Peak Element][Accepted] First Round: Iterative  solution

--- 162-Find-Peak-Element/firstRound.java
+++ 162-Find-Peak-Element/firstRound.java
@@ -6,7 +6,7 @@ public class Solution {
        int left = 0, right = nums.length-1;
        while (left < right){
            int mid = left + (right - left)/2;
-           if (nums[mid] < nums.[mid+1]){
+           if (nums[mid] < nums[mid+1]){
                left = mid+1;
            }else{
                right = mid;
