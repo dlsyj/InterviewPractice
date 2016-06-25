969f306c43a5e0f6eb01ddc23f2a0236733c60e0
dlsyj <dlsyj@users.noreply.github.com>
2016-06-25 14:55:45 -0400

    [118. Pascal's Triangle][Accepted] First Round:quick pick up

--- 118-Pascal's-Triangle/solution.java
+++ 118-Pascal's-Triangle/solution.java
@@ -4,8 +4,8 @@ public class Solution {
         List<Integer> eachLine = new ArrayList<Integer>();
         for (int i = 0; i < numRows; i++){
             eachLine.add(0,1);
-            for (int j = 1, j < eachLine.length; j++){
-                eachLine.set(j, eachLine.get(j) + eachLine.get(j+1))
+            for (int j = 1; j < eachLine.size()-1; j++){
+                eachLine.set(j, eachLine.get(j) + eachLine.get(j+1));
             }
             ans.add(new ArrayList<Integer>(eachLine));
         }
