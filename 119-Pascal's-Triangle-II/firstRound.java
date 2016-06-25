358bde36193a88fe42d2f47a4967b8713728c1f1
dlsyj <dlsyj@users.noreply.github.com>
2016-06-25 15:41:05 -0400

    [119. Pascal's Triangle II][Accepted] First Round: O(n) space, O(n^2) time

---
+++ 119-Pascal's-Triangle-II/firstRound.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < rowIndex+1; i++){
+            ans.add(1);
+            for (int j = i-1; j > 0; j--){
+                ans.set(j, ans.get(j) + ans.get(j-1));
+            } 
+        }
+        return ans;
+    }
+}
