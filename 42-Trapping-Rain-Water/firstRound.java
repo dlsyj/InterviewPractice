918c06476574c902a3b5c7c42564cb9be4841264
dlsyj <dlsyj@users.noreply.github.com>
2016-06-25 20:33:07 -0400

    [42. Trapping Rain Water][Accepted] First Round: Two pointers, compare the lower side and move to higher side

--- 42-Trapping-Rain-Water/firstRound.java
+++ 42-Trapping-Rain-Water/firstRound.java
@@ -1,25 +1,27 @@
 public class Solution {
     public int trap(int[] height) {
-        // Corner Case
-        if (height == null || height.length == 0){
-            return 0;
-        }
-        int max = 0;
-        int ans = 0;
-        int[] container = new int[height.length];
-        // First scan: from left to right
-        for (int i = 0; i < height.length; i++){
-            container[i] = max;
-            max = Math.max(max,height[i]);
-        }
-        // Second scan: from right to left
-        max = 0;
-        for (int i = height.length-1; i >= 0; i--){
-            // Find the lower MAX values from two sides
-            container[i] = Math.min(max,container[i]);
-            max = Math.max(max,height[i]);
-            ans += container[i]-height[i] > 0 ? container[i]-height[i] : 0;
-        }
-        return ans;
+       if (height == null || height.length == 0){
+           return 0;
+       }
+       int left = 0, right = height.length - 1;
+       int ans = 0;
+       while (left < right){
+           int min = Math.min(height[left], height[right]);
+           if (height[left] == min){
+               // Now min is lower boundary of pool
+               left ++;
+               while (left < right && height[left] < min){
+                   ans += min - height[left];
+                   left++;
+               }
+           }else{
+               right --;
+               while (left < right && height[right] < min){
+                   ans += min - height[right];
+                   right--;
+               }
+           }
+       }
+       return ans;
     }
 }
