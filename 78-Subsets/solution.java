63872177e774b06a5ea9001d67e3edde33dbd7f1
dlsyj <dlsyj@users.noreply.github.com>
2016-07-05 12:50:06 -0400

    [78. Subsets][Accepted] First Round:DFS without ending condition

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,24 +1,19 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         // Method 2: DFS/Backtracking
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        // corner case
-        if (nums == null || nums.length == 0){
-            return ans;
-        }
-        dfs(nums,ans,new ArrayList<Integer>(),0);
-        return ans;
+       List<List<Integer>> ans = new ArrayList<List<Integer>>();
+       if (nums == null || nums.length == 0){
+           return ans;
+       }
+       dfs(nums,ans,new ArrayList<Integer>(),0);
+       return ans;
     }
     public void dfs(int[] nums, List<List<Integer>> ans, ArrayList<Integer> path, int index){
-        // Exit Condition
-        if (index == nums.length){
-            ans.add(new ArrayList<Integer>(path));
-            return;
-        }else{
-            dfs(nums,ans,path,index+1); // current element is not in path
-            path.add(nums[index]);
-            dfs(nums,ans,path,index+1); // current element is in path
-            path.remove(path.size()-1); // remove the current element
+        ans.add(new ArrayList<Integer>(path));
+        for (int i = index; i < nums.length; i++){
+            path.add(nums[i]);
+            dfs(nums,ans,path,i+1);
+            path.remove(path.size()-1);
         }
     }
 }
