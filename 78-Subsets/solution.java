6e623fe3682970a6bbfa8a1dbffa43dc2f707409
dlsyj <dlsyj@users.noreply.github.com>
2016-07-05 11:02:09 -0400

    [78. Subsets][Compile Error] First Round:Bit Manipulation, without sort raw array

---
+++ 78-Subsets/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        // Method 1: Bit Manipulation
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        int length = nums.length;
+        int subsetNum = 1 << length;    // total number of subset
+        for (int i = 0; i < subsetNum; i++){
+            List<Integer> eachSubset = new ArrayList<Integer>();
+            for (int j = 0; j < length; j++){
+                if (i && (1<<j)){
+                    eachSubset.add(nums[j]);
+                }
+            }
+            ans.add(eachSubset);
+        }
+        return ans;
+    }
+}
