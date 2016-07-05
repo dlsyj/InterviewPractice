07fe1695bcea2f8fd0d29210373e5913149b63f3
dlsyj <dlsyj@users.noreply.github.com>
2016-07-05 11:23:12 -0400

    [78. Subsets][Accepted] First Round: DFS/Backtracking without sorting raw data

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,18 +1,24 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        // Method 1: Bit Manipulation
+        // Method 2: DFS/Backtracking
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        int length = nums.length;
-        int subsetNum = 1 << length;    // total number of subset
-        for (int i = 0; i < subsetNum; i++){
-            List<Integer> eachSubset = new ArrayList<Integer>();
-            for (int j = 0; j < length; j++){
-                if ((i&(1<<j)) != 0){
-                    eachSubset.add(nums[j]);
-                }
-            }
-            ans.add(eachSubset);
+        // corner case
+        if (nums == null || nums.length == 0){
+            return ans;
         }
+        dfs(nums,ans,new ArrayList<Integer>(),0);
         return ans;
+    }
+    public void dfs(int[] nums, List<List<Integer>> ans, ArrayList<Integer> path, int index){
+        // Exit Condition
+        if (index == nums.length){
+            ans.add(new ArrayList<Integer>(path));
+            return;
+        }else{
+            dfs(nums,ans,path,index+1); // current element is not in path
+            path.add(nums[index]);
+            dfs(nums,ans,path,index+1); // current element is in path
+            path.remove(path.size()-1); // remove the current element
+        }
     }
 }
