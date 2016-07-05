11ec35585fc1c152dc4334a6f22c4b06a84a1f5c
dlsyj <dlsyj@users.noreply.github.com>
2016-07-05 14:59:17 -0400

    [120. Triangle][Runtime Error] First Round:from bottom to top, DP with O(n) space

---
+++ 120-Triangle/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null){
+            return 0;
+        }
+        // create int[] to store the intermidiate value
+        int[] ans = new int[triangle.size()];
+        for (int i = 0; i < ans.length; i++){
+            // store the bottom line of triangle
+            ans[i] = triangle.get(triangle.size()-1).get(i);
+        }
+        for (int i = triangle.size()-2; i >= 0; i--){
+            for (int j = 0; j < i; j++){
+                ans[j] = Math.min(ans[j],ans[j-1])+triangle.get(i).get(j);
+            }
+        }
+        return ans[0];
+        
+    }
+}
