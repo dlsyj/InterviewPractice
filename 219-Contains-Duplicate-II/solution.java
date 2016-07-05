3d4d79f5d1455e26fe1ff16bdab87131a0fc636e
dlsyj <dlsyj@users.noreply.github.com>
2016-07-05 16:33:23 -0400

    [219. Contains Duplicate II][Accepted] First Round:HashSet and sliding window

--- 219-Contains-Duplicate-II/solution.java
+++ 219-Contains-Duplicate-II/solution.java
@@ -1,15 +1,12 @@
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        Set<Integer> set = new HashSet<Integer>();
         for (int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                if (i-map.get(nums[i]) <= k){
-                    return true;
-                }else{
-                    map.put(nums[i],i);
-                }
-            }else{
-                map.put(nums[i],i);
+            if(i > k) {
+                set.remove(nums[i-k-1]);
+            }
+            if(!set.add(nums[i])){
+                return true;
             }
         }
         return false;
