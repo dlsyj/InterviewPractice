aee6b744f1b608a8a24dec5265ba4cd5ec2048f6
dlsyj <dlsyj@users.noreply.github.com>
2016-07-06 18:21:05 -0400

    [220. Contains Duplicate III][Runtime Error] quick pick up

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,19 +1,19 @@
 import java.util.SortedSet; // the return value of subset is SortedSet(Interface) type
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-       // subset in TreeSet will return nums [lower,upper)
-       // Corner Case
        if (nums == null || nums.length < 2 || k < 1 || t < 0){
            return false;
        }
        SortedSet<Long> windowNumSet = new TreeSet<Long>();
        for (int i = 0; i < nums.length; i++){
-           SortedSet<Long> setReturned = windowNumSet.subSet((long)nums[i] - t, (long)nums[i] + t + 1);    // [lower,upper)
+           SortedSet<Long> setReturned = windowNumSet.subSet((long)nums[i] - t, (long)nums[i] + t + 1);
            if (!setReturned.isEmpty()){
                return true;
            }
-           if (i >= k) windowNumSet.remove((long)nums[i-k]);
-           windowNumSet.add((long)nums[i]);
+           if (i >= k){
+               windowNumSet.remove((long)nums[i-k]);
+           }
+           windowNumSet.add((long)nums[i-k]);
        }
        return false;
     }
