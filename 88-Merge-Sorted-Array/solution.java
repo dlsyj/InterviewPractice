c4164980d06bac70699d00d8526320f528ebb659
dlsyj <dlsyj@users.noreply.github.com>
2016-07-06 19:15:16 -0400

    [88. Merge Sorted Array][Accepted] First Round:

--- 88-Merge-Sorted-Array/solution.java
+++ 88-Merge-Sorted-Array/solution.java
@@ -1,12 +1,9 @@
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         // Corner Case
-        if (nums1 == null || nums2 == null || m < n || n != nums2.length){
-            return;
-        }
-        if (m != nums1.length){
-            nums1 = nums2;
-        }
+        // if (nums1 == null || nums2 == null){
+        //     return;
+        // }
          // keep three indexes: last index in nums1, last index in nums2, last index in result
         int index1 = m - 1, index2 = n - 1, indexAns = m + n - 1;
         while (index1 >= 0 && index2 >= 0){
