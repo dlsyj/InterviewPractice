cff067aa58b338975bb455762b10e4c4e9b58eb2
dlsyj <dlsyj@users.noreply.github.com>
2016-07-07 15:31:04 -0400

    [34. Search for a Range][Accepted] First Round:Binary search, firstGreaterEqual

--- 34-Search-for-a-Range/solution.java
+++ 34-Search-for-a-Range/solution.java
@@ -2,18 +2,17 @@ public class Solution {
     public int[] searchRange(int[] nums, int target) {
         int start = firstGreaterEqual(nums,target);
         if (start == nums.length || nums[start] != target){
-            return new int[] {-1,-1};
+            return new int[]{-1,-1};
         }
-        return new int[] {start,firstGreaterEqual(nums,target+1)-1};
+        return new int[]{start,firstGreaterEqual(nums,target+1)-1};
     }
-    
     public int firstGreaterEqual(int[] nums, int target){
         int left = 0, right = nums.length;
         while (left < right){
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] < target){
-                left = mid+1;
-            }else{
+                left = mid + 1;
+            } else {
                 right = mid;
             }
         }
