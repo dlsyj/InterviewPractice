3f5c66c5138b928729625600b4f52e7796d958ff
dlsyj <dlsyj@users.noreply.github.com>
2016-07-08 18:12:31 -0400

    [229. Majority Element II][Accepted] First Round: HashMap

---
+++ 229-Majority-Element-II/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        List<Integer> ans = new ArrayList<Integer>();
+        if (nums == null || nums.length == 0){
+            return ans;
+        }
+        for (int num : nums){
+            if (!map.containsKey(num)){
+                map.put(num,1);
+            }else{
+                map.put(num,map.get(num)+1);
+            }
+        }
+        for (int key : map.keySet()){
+            if (map.get(key) > Math.floor(nums.length/3)){
+                ans.add(key);
+            }
+        }
+        return ans;
+    }
+}
