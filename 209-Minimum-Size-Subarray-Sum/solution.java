f82df1585fd8a0a6e4d81e2a0fa31173dafb289d
dlsyj <dlsyj@users.noreply.github.com>
2016-07-08 15:40:04 -0400

    [209. Minimum Size Subarray Sum][Compile Error] First Round: set up accumulative array and binary search right index

--- 209-Minimum-Size-Subarray-Sum/solution.java
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     int minSubArrayLen(int s, int[] nums) {
-        int len = nums.size(), sums[len + 1] = {0}, res = len + 1;
+        int len = nums.size();
+        int[] sums = new int[len+1];
+        int res = len + 1;
         for (int i = 1; i < len + 1; ++i) sums[i] = sums[i - 1] + nums[i - 1];
         for (int i = 0; i < len + 1; ++i) {
             int right = searchRight(i + 1, len, sums[i] + s, sums);
