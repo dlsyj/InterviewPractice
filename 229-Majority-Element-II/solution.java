f79e413d313ee1a1dbbc8f3e229b50bcc24853ce
dlsyj <dlsyj@users.noreply.github.com>
2016-07-08 21:07:08 -0400

    [229. Majority Element II][Compile Error] First Round:Moore Voting algorithm

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,37 +1,50 @@
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        if (nums.length==0) return res;
-        
-        int count[] = new int[2];        
-        int x[] = new int[2];       
-   
-        x[0] = 0; x[1] = 1;        
-        for (int i = 0; i < nums.length; i++) {
-        	if (x[0] == nums[i])
-        		count[0]++;
-        	else if (x[1] == nums[i])
-        		count[1]++;
-        	else if (count[0] == 0) {
-        		x[0] = nums[i];
-        		count[0] = 1;
-        	} else if (count[1] == 0) {
-        		x[1] = nums[i];
-        		count[1] = 1;
-        	} else {
-        		count[0]--;
-        		count[1]--;        		
-        	}
-        }
-        
-        Arrays.fill(count, 0);
-        for (int i : nums) {// Count again for x1, x2
-    	if (i == x[0]) count[0]++;
-    	else if (i == x[1]) count[1]++;
-    }
-    for (int j = 0; j < 2; j++) {
-    	if (count[j] > nums.length/3 && !res.contains(x[j])) res.add(x[j]);
-    }        
-    return res;
+        List<Integer> ans = new ArrayList<Integer>();
+    	if (nums == null || nums.length == 0){
+    		return ans;
+    	}
+    	int[] count = new int[2];
+    	int[] temp = new int[2];
+    
+    	// get top 2 frequent elements
+    	temp[0] = 0;
+    	temp[1] = 1;
+    	for (int num : nums){
+    		if (count[0] == 0){
+    			temp[0] = num;
+    			count[0] = 1;
+    		}else if (count[1] == 0){
+    			temp[1] = num;
+    			count[1] = 1;
+    		}else if (num == temp[0]){
+    			count[0]++;
+    		}else if (num == temp[1]){
+    			count[1]++;
+    		}else{
+    			count[0]--;
+    			count[1]--;
+    		}
+    	}
+    
+    	int[] reCount = new int[2];
+    	for (int num : nums){
+    		if (num == temp[0]){
+    			reCount[0]++;
+    			if (reCount[0] > Math.floor(nums.length/3) && !ans.contains(num)){
+    				ans.add(num);
+    			}
+    		}else if (num == temp[1]){
+    			reCount[1]++;
+    			if (reCount[1] > Math.floor(nums.length/3) && !ans.contains(num)){
+    				ans.add(num);
+    			}
+    		}
+    	}
+    	for (int j = 0; j < 2; j++) {
+    	    if (reCount[j] > nums.length/3 && !res.contains(x[j])) res.add(x[j]);
+        }  
+    	
+    	return ans;
     }
 }
