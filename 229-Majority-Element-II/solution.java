caec1b7be2ce5ddb3282b76df5d5d1a2661f82c5
dlsyj <dlsyj@users.noreply.github.com>
2016-07-08 21:03:18 -0400

    [229. Majority Element II][Accepted] First Round:Moore Voting algorithm

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,20 +1,37 @@
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        List<Integer> ans = new ArrayList<Integer>();
-        if (nums == null || nums.length == 0){
-            return ans;
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if (nums.length==0) return res;
+        
+        int count[] = new int[2];        
+        int x[] = new int[2];       
+   
+        x[0] = 0; x[1] = 1;        
+        for (int i = 0; i < nums.length; i++) {
+        	if (x[0] == nums[i])
+        		count[0]++;
+        	else if (x[1] == nums[i])
+        		count[1]++;
+        	else if (count[0] == 0) {
+        		x[0] = nums[i];
+        		count[0] = 1;
+        	} else if (count[1] == 0) {
+        		x[1] = nums[i];
+        		count[1] = 1;
+        	} else {
+        		count[0]--;
+        		count[1]--;        		
+        	}
         }
-        for (int num : nums){
-            if (!map.containsKey(num)){
-                map.put(num,1);
-            }else{
-                map.put(num,map.get(num)+1);
-            }
-            if (map.get(key) > Math.floor(nums.length/3)){
-                ans.add(key);
-            }
-        }
-        return ans;
+        
+        Arrays.fill(count, 0);
+        for (int i : nums) {// Count again for x1, x2
+    	if (i == x[0]) count[0]++;
+    	else if (i == x[1]) count[1]++;
+    }
+    for (int j = 0; j < 2; j++) {
+    	if (count[j] > nums.length/3 && !res.contains(x[j])) res.add(x[j]);
+    }        
+    return res;
     }
 }
