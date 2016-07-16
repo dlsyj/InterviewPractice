f72518320c98240f6ded3bb939a57a684660d225
dlsyj <dlsyj@users.noreply.github.com>
2016-07-16 11:36:42 -0400

    [200. Number of Islands][Compile Error] First Round:DFS

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -6,7 +6,7 @@ public class Solution {
         int numOfIslands = 0;
         for (int i = 0; i < grid.length; i ++){
             for (int j = 0; j < grid[0].length; j ++){
-                if (grid[i][j] != '1'){
+                if (grid[i][j] != ''){
                     continue;
                 }
                 numOfIslands ++;
@@ -16,16 +16,16 @@ public class Solution {
         return numOfIslands;
     }
     
-    public void dfs(char[][] grid, int i, int j){
+    public void dfs(boolean[][] grid, int i, int j){
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length){
             return;
         }
-        if (grid[i][j] == '1'){
-            grid[i][j] = '2';
+        if (grid[i][j] == true){
+            grid[i][j] = false;
+            dfs(grid,i-1,j);
+            dfs(grid,i+1,j);
+            dfs(grid,i,j-1);
+            dfs(grid,i,j+1);
         }
-        dfs(grid,i - 1,j);
-        dfs(grid,i + 1,j);
-        dfs(grid,i,j - 1);
-        dfs(grid,i,j + 1);
     }
 }
