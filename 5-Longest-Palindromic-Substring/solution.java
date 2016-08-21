5304f1f4cf3348515ce65a5cfc7c3b48b908025b
dlsyj <dlsyj@users.noreply.github.com>
2016-08-21 16:33:51 -0400

    [5. Longest Palindromic Substring][Runtime Error] First Round: iterate each element and extend palindrome to two sides

---
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    private int index, maxLen;
+    
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2){
+            return s;
+        }
+        for (int i=0; i < len; i++){
+            extendPalindrome(s,i,i); // length of s is odd
+            extendPalindrome(s,i,i+1);// length of s is even
+        }
+        return s.substring(index,index+maxLen);
+    }
+    
+    public void extendPalindrome(String s, int left, int right){
+        while (left >= 0 && right <s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        if (maxLen < right-left+1){
+            index = left;
+            maxLen = right-left+1;
+        }
+    }
+}
