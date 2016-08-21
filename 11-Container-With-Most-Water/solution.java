563af42330d4ef897790653ab4d12ba95a6c2683
dlsyj <dlsyj@users.noreply.github.com>
2016-08-21 19:50:33 -0400

    [11. Container With Most Water][Accepted] First Round:

--- 11-Container-With-Most-Water/solution.java
+++ 11-Container-With-Most-Water/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int maxArea(int[] height) {
         int maxAns = 0;
         int left = 0, right = height.length - 1;
-        while (left <= right){
+        while (left < right){
             maxAns = Math.max(maxAns,Math.min(height[left],height[right])*(right-left));
             if (height[left] < height[right]){
                 left++;
