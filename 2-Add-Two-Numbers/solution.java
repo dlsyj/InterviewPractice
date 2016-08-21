da340c35f15a4088363bd9335eeba44fc5a06b8b
dlsyj <dlsyj@users.noreply.github.com>
2016-08-20 21:54:06 -0400

    [2. Add Two Numbers][Accepted] First Round: two linkedlist move forward together to create a dummy pointer

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -12,22 +12,23 @@ public class Solution {
         ListNode c2 = l2;
         ListNode dummy = new ListNode(0);
         ListNode d = dummy;
-        int sum = 0;
-        while (c1 != null || c2 != null) {
-            sum /= 10;
-            if (c1 != null) {
-                sum += c1.val;
+        int num = 0;
+        while (c1 != null || c2 != null){
+            num /= 10;
+            if (c1 != null){
+                num += c1.val;
                 c1 = c1.next;
             }
-            if (c2 != null) {
-                sum += c2.val;
+            if (c2 != null){
+                num += c2.val;
                 c2 = c2.next;
             }
-            d.next = new ListNode(sum % 10);
+            d.next = new ListNode(num%10);
             d = d.next;
         }
-        if (sum / 10 == 1)
+        if (num /10 == 1){
             d.next = new ListNode(1);
+        }
         return dummy.next;
     }
 }
