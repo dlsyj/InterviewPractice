04a5258e43420a690d6404208170200d3ee65a38
dlsyj <dlsyj@users.noreply.github.com>
2016-08-22 00:11:41 -0400

    [15. 3Sum][Accepted] First Round:sort and then use two pointers

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -4,29 +4,34 @@ public class Solution {
         if (nums == null || nums.length < 3){
             return ans;
         }
-        Arrays.sort(nums);  // O(nlogn)
-        for (int i = 0; i < nums.length; i++){
-            if (i > 0 && nums[i] == nums[i-1]){ // skip the duplicated one
-               continue;
-            } else {
-                int target = 0-nums[i];
-                int left = i+1, right = nums.length-1;
-                while (left < right){
-                    if (nums[left] + nums[right] == target){
-                        ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                        while (left < right && nums[left] == nums[left+1]){ // skip the duplicated one
-                            left++;
-                        }
-                        while (left < right && nums[right] == nums[right-1]){   // skip the duplicated one
-                            right --;
-                        }
+        Arrays.sort(nums);  // SORT firstly
+        for (int i = 0; i < nums.length - 2; i++){
+            if (i > 0 && nums[i] == nums[i-1]){
+                continue; // to skip duplicated numbers, e.g [0,0,0]
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right){
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == 0){
+                    List<Integer> temp = new ArrayList<Integer>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    ans.add(temp);
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]){  // to skip duplicated numbers
                         left++;
-                        right--;
-                    }else if (nums[left] + nums[right] < target){
-                        left++;
-                    }else{
+                    }
+                    while (left < right && nums[right] == nums[right + 1]){   // to skip duplicated numbers
                         right--;
                     }
+                }else if (sum < 0){
+                    left ++;
+                }else{
+                    right--;
                 }
             }
         }
