f95624f5dc90d04a443d452b149a86b207e4b271
dlsyj <dlsyj@users.noreply.github.com>
2016-08-22 12:05:58 -0400

    [24. Swap Nodes in Pairs][Accepted] First Round: iteration

--- 24-Swap-Nodes-in-Pairs/solution.java
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -11,9 +11,19 @@ public class Solution {
         if (head == null || head.next == null){
             return head;
         }
-        ListNode n = head.next;
-        head.next = swapPairs(head.next.next);
-        n.next = head;
-        return n;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        head = dummy;
+        while (head.next != null && head.next.next != null){
+            ListNode n1 = head.next, n2 = head.next.next;
+            // covert from "head->n1->n2" to "head->n2->n1"
+            head.next = n2;
+            n1.next = n2.next;
+            n2.next = n1;
+            // move to next pair
+            head = n1;
+        }
+        return dummy.next;
     }
 }
