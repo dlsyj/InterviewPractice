8a2324457301f993f76692f555368d20d86a09ce
dlsyj <dlsyj@users.noreply.github.com>
2016-08-22 00:36:48 -0400

    [16. 3Sum Closest][Accepted] First Round:

--- 16-3Sum-Closest/solution.java
+++ 16-3Sum-Closest/solution.java
@@ -1,29 +1,24 @@
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
         if (nums == null || nums.length < 3){
-            return 0;
+            return -1;
         }
-        Arrays.sort(nums);  // Easy to forget
-        int minDiff = Integer.MAX_VALUE; 
-        int closestSum = 0;
+        Arrays.sort(nums);  // SORT firstly
+        int bestSum = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length; i++){
-            int left = i+1, right = nums.length-1;
+            int left = i + 1, right = nums.length - 1;
             while (left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                int diff = Math.abs(target-sum);
-                if (diff < minDiff){
-                    minDiff = diff;
-                    closestSum = sum;
+                if (Math.abs(target-sum) < Math.abs(target-bestSum)){
+                    bestSum = sum;
                 }
                 if (sum < target){
-                    left ++;
-                }else if (sum > target){
-                    right --;
+                    left++;
                 }else{
-                    return sum;
+                    right--;
                 }
             }
         }
-        return closestSum;
+        return bestSum;
     }
 }
