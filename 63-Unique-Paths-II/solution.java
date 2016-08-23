01a8c2a3dd4ec3c2aa7ddc4893e7c56bc7523c48
dlsyj <dlsyj@users.noreply.github.com>
2016-08-23 19:22:12 -0400

    [63. Unique Paths II][Compile Error] First Round:

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -1,21 +1,28 @@
 public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0){
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
             return 0;
         }
-        int[] ans = new int[obstacleGrid[0].length];
-        ans[0] = 1;
-        for (int i=0; i < obstacleGrid.length; i++){
-            for (int j=0; j < obstacleGrid[0].length; j++){
-                if (obstacleGrid[i][j] == 1){
-                    ans[j] = 0;
-                } else {
-                    if (j > 0){
-                        ans[j] += ans[j-1];
-                    }
+        
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] paths = new int[m][n];
+        
+        for (int i = 0; i < m; i++){
+            paths[i][0] = obstacleGrid[i][0] != 0 ? break:1;
+        }
+        for (int j = 0; j < n; j++){
+            path[0][j] = obstacleGrid[0][j] != 0 ? break:1;
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                if (obstacleGrid[i][j] == 0){
+                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                }else{
+                    paths[i][j] = 0;
                 }
             }
         }
-        return ans[obstacleGrid[0].length-1];
+        return paths[m-1][n-1];
     }
 }
