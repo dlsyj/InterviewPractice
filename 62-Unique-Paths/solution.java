ab554c9d08a8bdd3c4c0d8e5bc26e53b721e5346
dlsyj <dlsyj@users.noreply.github.com>
2016-08-23 19:06:31 -0400

    [62. Unique Paths][Compile Error] First Round: DP solution

---
+++ 62-Unique-Paths/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        // DP
+        if (m == 0 || n == 0){
+            return 0;
+        }
+        
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++){
+            result[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++){
+            result[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                result[i][j] = result[i-1][j] + result[i][j-1];
+            }
+        }
+        return sum[m-1][j-1];
+    }
+}
