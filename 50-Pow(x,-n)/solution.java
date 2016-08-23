52c48dbdaba2b7a472ca67a77e39d7a71410162c
dlsyj <dlsyj@users.noreply.github.com>
2016-08-23 16:52:37 -0400

    [50. Pow(x, n)][Accepted] First Round: iteration

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public double myPow(double x, int n) {
         double ans = 1;
-        long absN = Math.Abs((long)n);
+        long absN = Math.abs((long)n);
         while(absN > 0) {
             if((absN&1)==1) ans *= x;
             absN >>= 1;
