55a4ad16d66667ae7e83e3f5bc6724f0875c3987
dlsyj <dlsyj@users.noreply.github.com>
2016-08-23 00:45:01 -0400

    [49. Group Anagrams][Accepted] First Round:

---
+++ 49-Group-Anagrams/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // <sorted string, raw string>
+        HashMap<String,ArrayList<String>> map = new HashMap<String,ArrayList<String>>();
+        List<List<String>> ans = new ArrayList<List<String>>();
+        if (strs == null || strs.length == 0){
+            return ans;
+        }
+        for (int i = 0; i < strs.length; i++){
+            char[] rawArr = strs[i].toCharArray();
+            Arrays.sort(rawArr);
+            String sortedStr = String.valueOf(rawArr);
+            if (map.containsKey(sortedStr)){
+                map.get(sortedStr).add(strs[i]);
+            }else{
+                ArrayList<String> list = new ArrayList<String>();
+                list.add(strs[i]);
+                map.put(sortedStr,list);
+            }
+        }
+        for (String each : map.keySet()){
+            ans.add(map.get(each));
+        }
+        return ans;
+    }
+}
