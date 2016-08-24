80b5252a838bf13a699dbd0b6fffccab15a8872f
dlsyj <dlsyj@users.noreply.github.com>
2016-08-23 22:49:26 -0400

    [69. Sqrt(x)][Accepted] First Round: binary search

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -4,14 +4,14 @@ public class Solution {
             return x;
         }
         int left = 1, right = x;
-        while(left <= right){
+        while(left < right){
             int mid = left + (right - left)/2;
             if (mid <= x/mid){
                 left = mid + 1;
             } else {
-                right = mid - 1;
+                right = mid;
             }
         }
-        return mid;
+        return left - 1;
     }
 }
