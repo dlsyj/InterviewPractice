7597f33e0926ab268caf7e4e57a91205c432b272
dlsyj <dlsyj@users.noreply.github.com>
2016-08-24 00:58:03 -0400

    [79. Word Search][Accepted] First Round:

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,33 +1,28 @@
 public class Solution {
-
-    boolean[][] visited;
-    
     public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
+        // DFS
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board[0].length; j++){
-                if (board[i][j] == word.charAt(0) && dfs(board,i,j,0,word)){
-                    return true;
+                if (board[i][j] == word.charAt(0)){
+                    if (find(board,i,j,word,0)){
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
     
-    public boolean dfs(char[][]board,int i, int j, int index, String word){
+    public boolean find(char[][] board, int i, int j, String word, int index){
         if (index == word.length()){
             return true;
         }
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index) ||visited[i][j]){
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)){
             return false;
         }
-        visited[i][j] = true;
-        boolean aroundFound = dfs(board,i-1,j,index+1,word)||
-                     dfs(board,i+1,j,index+1,word)||
-                     dfs(board,i,j-1,index+1,word)||
-                     dfs(board,i,j+1,index+1,word);
-        // Attention here, must change visited[i][j] to false;
-        visited[i][j] = false;
-        return aroundFound;
+        board[i][j] = '*';  // temporarily replace
+        boolean result = find(board, i+1, j, word, index+1) || find(board, i-1, j, word, index+1) || find(board, i, j+1, word, index+1) || find(board, i, j-1, word, index+1);
+        board[i][j] = word.charAt(index);   // Notice to turn back
+        return result;
     }
 }
