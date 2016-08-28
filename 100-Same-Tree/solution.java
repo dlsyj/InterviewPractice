76797d147e69623828ebc4334edb2c3407c718a4
dlsyj <dlsyj@users.noreply.github.com>
2016-08-28 14:20:17 -0400

    [100. Same Tree][Accepted] First Round: Iterative solution

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -9,12 +9,40 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null){
-            return true;
+        // Iterative Solution
+        Stack<TreeNode> stack_p = new Stack<TreeNode>();
+        Stack<TreeNode> stack_q= new Stack<TreeNode>();
+        if (p != null){
+            stack_p.push(p);
         }
-        if (p == null || q == null){
-            return false;
+        if (q != null){
+            stack_q.push(q);
         }
-        return p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        while (!stack_p.isEmpty() && !stack_q.isEmpty()){
+            TreeNode cur_p = stack_p.pop();
+            TreeNode cur_q = stack_q.pop();
+            if (cur_p.val != cur_q.val){
+                return false;
+            }
+            if (cur_p.left != null){
+                stack_p.push(cur_p.left);
+            }
+            if (cur_q.left != null){
+                stack_q.push(cur_q.left);
+            } 
+            if (stack_p.size() != stack_q.size()){
+                return false;
+            }
+            if (cur_p.right != null){
+                stack_p.push(cur_p.right);
+            }
+            if (cur_q.right != null){
+                stack_q.push(cur_q.right);
+            }
+            if (stack_p.size() != stack_q.size()){
+                return false;
+            }
+        }
+        return stack_p.size() == stack_q.size();
     }
 }
