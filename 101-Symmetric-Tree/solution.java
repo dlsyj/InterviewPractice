0877b1794a4f9da3f40c5fc4cab87f1e5b1f8ddb
dlsyj <dlsyj@users.noreply.github.com>
2016-08-28 15:02:57 -0400

    [101. Symmetric Tree][Accepted] First Round: Recursive Solution

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -9,12 +9,16 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null){
-            return true;
+        return root == null || helper(root.left, root.right);
+    }
+    
+    public boolean helper(TreeNode left, TreeNode right){
+        if (left == null || right == null){
+            return left == right;
         }
-        if (root.left == null && root.right == null){
-            return true;
+        if (left.val != right.val){
+            return false;
         }
-        return root.left.val == root.right.val && isSymmetric(root.left) && isSymmetric(root.right);
+        return helper(left.left,right.right) && helper(left.right,right.left);
     }
 }
