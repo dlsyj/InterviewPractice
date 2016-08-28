45867bbd557266eb718177a50c388f982e23c39c
dlsyj <dlsyj@users.noreply.github.com>
2016-08-28 14:00:15 -0400

    [98. Validate Binary Search Tree][Accepted] First Round: Iterative solution

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -9,12 +9,24 @@
  */
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        // Interation Solution
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode curr = root;
+        TreeNode pre = null;
+        while (!stack.isEmpty() || curr != null){
+            if (curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                TreeNode node = stack.pop();
+                if (pre != null && node.val <= pre.val){
+                    return false;
+                }
+                pre = node;
+                curr = node.right;
+            }
+        }
+        return true;
     }
-    
-    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
-        if (root == null) return true;
-        if (root.val >= maxVal || root.val <= minVal) return false;
-        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
-    }
+
 }
