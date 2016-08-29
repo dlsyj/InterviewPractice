24149c4cb3b99ea791cbe38baeec5b1dae929326
dlsyj <dlsyj@users.noreply.github.com>
2016-08-29 13:05:23 -0400

    [102. Binary Tree Level Order Traversal][Accepted] First Round: DFS recursive solution

--- 102-Binary-Tree-Level-Order-Traversal/solution.java
+++ 102-Binary-Tree-Level-Order-Traversal/solution.java
@@ -9,26 +9,21 @@
  */
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        // DFS
         List<List<Integer>> wraplist = new ArrayList<List<Integer>>();
-        if (root == null){
-            return wraplist;
-        }
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelNum = queue.size();
-            List<Integer> sublist = new ArrayList<Integer>();
-            for (int i = 0; i < levelNum; i++){
-                if (queue.peek().left != null){
-                    queue.offer(queue.peek().left);
-                }
-                if (queue.peek().right != null){
-                    queue.offer(queue.peek().right);
-                }
-                sublist.add(queue.poll().val);
-            }
-            wraplist.add(sublist);
-        }
+        helper(wraplist,root,0);
         return wraplist;
+    }
+    
+    public void helper(List<List<Integer>> wraplist, TreeNode root, int height){
+        if (root == null){
+            return;
+        }
+        if (height >= wraplist.size()){
+            wraplist.add(new ArrayList<Integer>());
+        }
+        wraplist.get(height).add(root.val);
+        helper(wraplist,root.left,height+1);
+        helper(wraplist,root.right,height+1);
     }
 }
