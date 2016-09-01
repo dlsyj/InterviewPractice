2577113b8918af43c868598cfb604a2f00053e4a
dlsyj <dlsyj@users.noreply.github.com>
2016-09-01 09:21:58 -0400

    [111. Minimum Depth of Binary Tree][Accepted] First Round:

---
+++ 111-Minimum-Depth-of-Binary-Tree/solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        int result = (left == 0 || right == 0)? left+right+1:Math.min(left,right)+1;
+        return result;
+    }
+}
