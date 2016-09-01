1c4aa84b1b9c6c1dea1dd977e4d44e5b711ae2e0
dlsyj <dlsyj@users.noreply.github.com>
2016-09-01 08:42:39 -0400

    [105. Construct Binary Tree from Preorder and Inorder Traversal][Runtime Error] First Round:

--- 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
@@ -9,22 +9,26 @@
  */
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length - 1, preorder, inorder);
-    }
-    
-    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-        if (preStart > preorder.length - 1 || inStart > inEnd) {
+        if (preorder.length != inorder.length){
             return null;
         }
-        TreeNode root = new TreeNode(preorder[preStart]);
-        int inIndex = 0; // Index of current root in inorder
-        for (int i = inStart; i <= inEnd; i++) {
-            if (inorder[i] == root.val) {
-                inIndex = i;
+        return helper(preorder,0,inorder,0,inorder.length);
+    }
+    
+    public TreeNode helper(int[] preorder, int preStart, int[] inorder, int inStart, int inEnd){
+        if (preStart >= preorder.length || inStart >= inEnd){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        int index = 0;
+        for (int i = inStart; i < inEnd; i++){
+            if (root.val == inorder[i]){
+                index = i;
+                break;
             }
         }
-        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        root.left = helper(preorder,preStart+1,inorder,inStart,index-1);
+        root.right = helper(preorder,preStart+index-inStart,inorder,index+1,inEnd);
         return root;
     }
 }
