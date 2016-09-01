4297b76c1278dbb2b641fd1dde958f50aacf06d5
dlsyj <dlsyj@users.noreply.github.com>
2016-09-01 09:12:34 -0400

    [110. Balanced Binary Tree][Accepted] First Round:

---
+++ 110-Balanced-Binary-Tree/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if (Math.abs(left-right) > 1){
+            return false;
+        } else {
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+    
+    public int getDepth(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        return Math.max(getDepth(root.left),getDepth(root.right))+1;
+    }
+}
