f670a85e9a279b50f0a590f6b8deb4ce23b0f740
dlsyj <dlsyj@users.noreply.github.com>
2016-09-16 21:53:37 -0400

    [239. Sliding Window Maximum][Runtime Error] First Round:

---
+++ 239-Sliding-Window-Maximum/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null ||  nums.length == 0){
+            return new int[0];
+        }
+        int len = nums.length;
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        int[] result = new int[len - k + 1];
+        int resultIndex = 0;
+        for (int i = 0; i < nums.length; i ++){
+            // remove elements whose index are out of range k
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1){
+                deque.pollFirst();
+            }
+            // compare last element and remove all elements less than it in range k
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]){
+                deque.pollLast();
+            }
+            deque.addFirst(nums[i]);
+            if (i >= k - 1){
+                result[resultIndex++] = deque.peek();
+            }
+        }
+        return result;
+    }
+}
