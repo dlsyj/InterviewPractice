83019bee13514c2ed7a4dd86cca17afb913e3555
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 17:27:48 -0400

    [47. Permutations II][Accepted] First Round:

---
+++ 47-Permutations-II/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0){
+            return result;
+        }
+        List<Integer> first = new ArrayList<Integer>();
+        first.add(nums[0]);
+        result.add(first);
+        for (int i = 1; i < nums.length; i ++){
+            List<List<Integer>> newResult = new ArrayList<List<Integer>>();
+            for (int j = 0; j <= i; j ++){
+                for(List<Integer> each : result){
+                    List<Integer> newEach = new ArrayList<Integer>(each);
+                    newEach.add(j,nums[i]);
+                    newResult.add(newEach);
+                }
+            }
+            result = newResult;
+        }
+        HashSet<List<Integer>> distinctResult = new HashSet<List<Integer>>();
+        for (List<Integer> each : result){
+            distinctResult.add(each);
+        }
+        result.clear();
+        for (List<Integer> each : distinctResult){
+            result.add(each);
+        }
+        return result;
+    }
+}
