196de2a21486763c7c20f2a0a1b443a07833a31a
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 16:13:54 -0400

    [46. Permutations][Accepted] First Round:

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -4,19 +4,19 @@ public class Solution {
         if (nums == null || nums.length == 0){
             return result;
         }
-        List<Integer> current = new ArrayList<Integer>();
-        current.add(nums[0]);
-        result.add(current);
+        List<Integer> first = new ArrayList<Integer>();
+        first.add(nums[0]);
+        result.add(first);
         for (int i = 1; i < nums.length; i ++){
-            List<List<Integer>> newResult = new ArrayList<List<Integer>>();
+            List<List<Integer>> tempResult = new ArrayList<List<Integer>>();
             for (int j = 0; j <= i; j ++){
                 for (List<Integer> each : result){
-                    List<Integer> eachNewLine = new ArrayList<Integer>(each);
-                    eachNewLine.add(j,nums[i]);
-                    newResult.add(eachNewLine);
+                    List<Integer> newEach = new ArrayList<Integer>(each);
+                    newEach.add(j,nums[i]);
+                    tempResult.add(newEach);
                 }
             }
-            result = newResult;
+            result = tempResult;
         }
         return result;
     }
