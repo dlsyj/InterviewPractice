5988953461cc9acb4d6f8b498327d726f1f89707
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 23:07:50 -0400

    [75. Sort Colors][Compile Error] First Round:

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -3,24 +3,22 @@ public class Solution {
         if (nums == null || nums.length == 0){
             return;
         }
-        int leftPointer = 0, rightPointer = nums.length - 1, int i = 0;
-        while (i <= rightPointer){
+        int leftPointer = 0, rightPointer = nums.length - 1;
+        for (int i = 0; i < nums.length; i ++){
             if (nums[i] == 0){
                 // found 0, put it in the beginning
-                swap(nums[i], nums[leftPointer++]);
-                i ++;
+                int temp = nums[i];
+                nums[i] = nums[leftPointer];
+                nums[leftPointer] = temp;
+                leftPointer++
             }else if (nums[i] == 2){
                 // found 2, put it in the end
-                swap(nums[i], nums[rightPointer--]);
-            }else{
-                i ++;
+                int temp = nums[i];
+                nums[i] = nums[rightPointer];
+                nums[rightPointer] = temp;
+                i--; 
+                rightPointer--;
             }
         }
-    }
-    
-    public void swap(int index1, int index2){
-        int temp = index1;
-        index1 = index2;
-        index2 = temp;
     }
 }
