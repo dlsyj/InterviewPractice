8719a724fff0d3ffd6103ec2b0c1f00110979341
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 23:11:40 -0400

    [75. Sort Colors][Accepted] First Round:

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -4,18 +4,18 @@ public class Solution {
             return;
         }
         int leftPointer = 0, rightPointer = nums.length - 1;
-        for (int i = 0; i < nums.length;){
+        for (int i = leftPointer; i <= rightPointer;){
             if (nums[i] == 0){
                 int temp = nums[i];
                 nums[i] = nums[leftPointer];
                 nums[leftPointer] = temp;
                 i ++;
-                leftPointer++;
+                leftPointer ++;
             }else if (nums[i] == 2){
                 int temp = nums[i];
                 nums[i] = nums[rightPointer];
                 nums[rightPointer] = temp;
-                rightPointer--;
+                rightPointer --;
             }else{
                 i ++;
             }
