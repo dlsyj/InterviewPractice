382f7c86a87eeea51efd5f4e4c08ecd662213d70
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 23:46:30 -0400

    [79. Word Search][Compile Error] First Round:

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,10 +1,14 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
         // DFS
-        for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board[0].length; j++){
+        if (board == null || board.length == 0 || board[0].length == 0){
+            return false;
+        }
+        for (int i = 0; i < board.length; i ++){
+            for (int j = 0; j < board[0].length; j ++){
                 if (board[i][j] == word.charAt(0)){
-                    if (find(board,i,j,word,0)){
+                    boolean found = DFShelper(board, word, i, j, 0);
+                    if (found){
                         return true;
                     }
                 }
@@ -13,16 +17,19 @@ public class Solution {
         return false;
     }
     
-    public boolean find(char[][] board, int i, int j, String word, int index){
+    public boolean DFShelper(char[][] board, String word, int i , int j, int index){
         if (index == word.length()){
             return true;
         }
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)){
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length){
             return false;
         }
-        board[i][j] = '*';  // temporarily replace
-        boolean result = find(board, i+1, j, word, index+1) || find(board, i-1, j, word, index+1) || find(board, i, j+1, word, index+1) || find(board, i, j-1, word, index+1);
-        board[i][j] = word.charAt(index);   // Notice to turn back
-        return result;
+        boolean found ＝ false;
+        if (board[i][j] == word.charAt(index)){
+            board[i][j] = '*';
+            found = DFShelper(board, word, i - 1, j, index + 1) || DFShelper(board, word, i + 1, j, index + 1) || DFShelper(board, word, i, j - 1, index + 1) || DFShelper(board, word, i, j + 1, index + 1);
+            board[i][j] = word.charAt(index);
+        }
+        return found;
     }
 }
