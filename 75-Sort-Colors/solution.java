adfd9667e8c8934d812f7e844ce7fa9d5c7cdaa2
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 22:34:43 -0400

    [75. Sort Colors][Compile Error] First Round:

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -1,19 +1,14 @@
 public class Solution {
-    public void sortColors(int[] A) {
-        int j = 0, k = A.length -1;
+    public void sortColors(int[] nums) {
+        int j = 0, k = nums.length -1;
     for (int i=0; i <= k; i++) {
         if (A[i] == 0)
             swap(A[i], A[j++]);
         else if (A[i] == 2)
-            swap(A[i--], A[k--]);
+            swap(A[i], A[k--]);
     }
     }
     
-    public void swap(int index1, int index2){
-        int temp = index1;
-        index1 = index2;
-        index = temp;
-    }
     public void swap(int[] nums, int index1, int index2){
         int temp = nums[index1];
         nums[index1] = nums[index2];
