bdaacc560d1cfbb2775c2d7bc92f58d9466dde5e
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 23:10:16 -0400

    [75. Sort Colors][Runtime Error] First Round:

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -1,22 +1,20 @@
 public class Solution {
     public void sortColors(int[] nums) {
-        if (nums == null || nums.length == 0){
+        if (nums == null || nums.length < 2){
             return;
         }
         int leftPointer = 0, rightPointer = nums.length - 1;
         for (int i = 0; i < nums.length; i ++){
             if (nums[i] == 0){
-                // found 0, put it in the beginning
                 int temp = nums[i];
                 nums[i] = nums[leftPointer];
                 nums[leftPointer] = temp;
                 leftPointer++;
             }else if (nums[i] == 2){
-                // found 2, put it in the end
                 int temp = nums[i];
                 nums[i] = nums[rightPointer];
                 nums[rightPointer] = temp;
-                i--; 
+                i--;
                 rightPointer--;
             }
         }
