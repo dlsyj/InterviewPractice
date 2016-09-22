b6708f031ed5187301288d8a660046795a417b3d
dlsyj <dlsyj@users.noreply.github.com>
2016-09-21 20:55:15 -0400

    [55. Jump Game][Accepted] First Round:

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,14 +1,11 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        if (nums == null || nums.length == 0){
-            return false;
-        }
-        int maxReached = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (i > maxReached){
+        int max = 0;
+        for (int i = 0; i < nums.length; i ++){
+            if (i > max){
                 return false;
             }
-            maxReached = Math.max(nums[i]+i,maxReached);
+            max = Math.max(max, nums[i] + i);
         }
         return true;
     }
