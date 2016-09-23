b97c9589de4176632fd1d0a38f1293d23eaab289
dlsyj <dlsyj@users.noreply.github.com>
2016-09-23 01:16:50 -0400

    [101. Symmetric Tree][Accepted] First Round:

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -9,26 +9,16 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null){
-            return true;
+        return root == null || helper(root.left, root.right);
+    }
+    
+    public boolean helper(TreeNode left, TreeNode right){
+        if (left == null || right == null){
+            return left == right;
         }
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root.left);
-        stack.push(root.right);
-        while (!stack.isEmpty()){
-            TreeNode rightNode = stack.pop();
-            TreeNode leftNode = stack.pop();
-            if (leftNode == null && rightNode == null){
-                continue; 
-            }
-            if (leftNode == null || rightNode == null || leftNode.val != rightNode.val){
-                return false;
-            }
-            stack.push(leftNode.left);
-            stack.push(rightNode.right);
-            stack.push(leftNode.right);
-            stack.push(rightNode.left);
+        if (left.val != right.val){
+            return false;
         }
-        return true;
+        return helper(left.left,right.right) && helper(left.right,right.left);
     }
 }
