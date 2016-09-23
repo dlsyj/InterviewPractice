6a4e1f11994d3cd6f608306d49aefe9be74dd236
dlsyj <dlsyj@users.noreply.github.com>
2016-09-23 15:19:34 -0400

    [114. Flatten Binary Tree to Linked List][Runtime Error] First Round:

---
+++ 114-Flatten-Binary-Tree-to-Linked-List/solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null){
+            return;
+        }
+        TreeNode leftNode = root.left;
+        TreeNode rightNode = root.right;
+        flatten(rightNode);
+        flatten(leftNode);
+        root.left = null;
+        root.right = leftNode;
+        while (root != null){
+            root = root.right;
+        }
+        root.right = rightNode;
+    }
+}
