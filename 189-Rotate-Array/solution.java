bec55703a8cf1a93bd89068598552562b933fcb9
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 01:38:32 -0400

    [189. Rotate Array][Runtime Error] First Round:

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public void rotate(int[] nums, int k) {
 	    swap(nums, 0, nums.length - 1);
-	    swap(nums, 0, k);
-	    swap(nums, k + 1, nums.length - 1);
+	    swap(nums, 0, k - 1);
+	    swap(nums, k, nums.length - 1);
 	}
 	
 	public void swap(int[] nums, int start, int end){
