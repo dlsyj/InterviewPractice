6835a79b795a25e7e0345bc52c0b08e10077fe4c
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 12:26:18 -0400

    [198. House Robber][Accepted] First Round:

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -7,7 +7,7 @@ public class Solution {
         for (int i = 1; i <= nums.length; i ++){
             // current is NOT stolen
             dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
-            dp[i][1] = dp[i - 1][0] + nums[i];
+            dp[i][1] = dp[i - 1][0] + nums[i - 1];
         }
         return Math.max(dp[nums.length][1],dp[nums.length][0]);
     }
