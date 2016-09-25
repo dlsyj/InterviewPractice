2cab0d623926c67864354e9ab593f729f801e347
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 01:58:09 -0400

    [191. Number of 1 Bits][Compile Error] First Round:

--- 191-Number-of-1-Bits/solution.java
+++ 191-Number-of-1-Bits/solution.java
@@ -3,8 +3,8 @@ public class Solution {
     public int hammingWeight(int n) {
         int result = 0;
         while (n != 0){
-            result ++;
-            n = n & (n - 1);
+            result += n & 1;
+            n >> 1;
         }
         return result;
     }
