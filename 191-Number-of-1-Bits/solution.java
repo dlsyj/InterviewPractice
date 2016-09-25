936b40197a491eb80539cd3433485aabc76d633b
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 01:58:25 -0400

    [191. Number of 1 Bits][Time Limit Exceeded] First Round:

--- 191-Number-of-1-Bits/solution.java
+++ 191-Number-of-1-Bits/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         int result = 0;
         while (n != 0){
             result += n & 1;
-            n >> 1;
+            n >>= 1;
         }
         return result;
     }
