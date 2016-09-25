4b7f8b5fe53e94ced63ec7f393d9d6c21a3dae7d
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 12:39:13 -0400

    [198. House Robber][Runtime Error] First Round:

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -4,10 +4,12 @@ public class Solution {
             return 0;
         }
         int[][] dp = new int[nums.length + 1][2];
+        dp[0][1] = nums[0];
         for (int i = 1; i <= nums.length; i ++){
-            // current is NOT stolen
+            // if we don't rob the current room, we can select to rob or not to rob the last room
             dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
-            dp[i][1] = dp[i - 1][0] + nums[i - 1];
+            // the current room is stolen
+            dp[i][1] = dp[i - 1][0] + nums[i];
         }
         return Math.max(dp[nums.length][1],dp[nums.length][0]);
     }
