d7e161f586ccd2a69578fa901cff4d4c65776de4
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 01:50:42 -0400

    [191. Number of 1 Bits][Accepted] First Round:

--- 191-Number-of-1-Bits/solution.java
+++ 191-Number-of-1-Bits/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         int result = 0;
         while (n != 0){
             result ++;
-            n = n && (n - 1);
+            n = n & (n - 1);
         }
         return result;
     }
