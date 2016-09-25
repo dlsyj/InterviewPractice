1f25e42e42a763e3c32d436c22d79771f67cbe12
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 12:39:50 -0400

    [198. House Robber][Accepted] First Round:

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -4,12 +4,11 @@ public class Solution {
             return 0;
         }
         int[][] dp = new int[nums.length + 1][2];
-        dp[0][1] = nums[0];
         for (int i = 1; i <= nums.length; i ++){
             // if we don't rob the current room, we can select to rob or not to rob the last room
             dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
             // the current room is stolen
-            dp[i][1] = dp[i - 1][0] + nums[i];
+            dp[i][1] = dp[i - 1][0] + nums[i - 1];
         }
         return Math.max(dp[nums.length][1],dp[nums.length][0]);
     }
