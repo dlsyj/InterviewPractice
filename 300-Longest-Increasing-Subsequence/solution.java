c0d8a28df5758f1e0ba6d53b9e433cf9e1de4b6e
dlsyj <dlsyj@users.noreply.github.com>
2016-09-26 10:20:06 -0400

    [300. Longest Increasing Subsequence][Compile Error] First Round:

---
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int result = 0;
+        // Base case
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        // Track longest sequence length
+        int[] dp = new int[nums.length;
+        // Fill each position with value 1 in the array
+        for (int i = 0; i < nums.length; i ++){
+            dp[i] = 1;
+        }
+        // Mark one pointer at i. For each i, start from j = 0
+        for (int i = 1; i < nums.length; i ++){
+            for (int j = 0; j < i; j ++){
+                if (nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+                result = Math.max(result, dp[i]);
+            }
+        }
+        return result;
+    }
+}
