c215276e3fb65fd801a073810416523c438df2a0
dlsyj <dlsyj@users.noreply.github.com>
2016-09-25 20:36:29 -0400

    [218. The Skyline Problem][Accepted] First Round:

--- 218-The-Skyline-Problem/solution.java
+++ 218-The-Skyline-Problem/solution.java
@@ -4,24 +4,24 @@ public class Solution {
         List<int[]> result = new ArrayList<int[]>();
         List<int[]> height = new ArrayList<int[]>();
         for (int[] each : buildings){
-            height.add(new int[]{each[0],each[2]});
-            height.add(new int[]{each[1],-each[2]});
+            height.add(new int[]{each[0],each[2]}); // add starting point
+            height.add(new int[]{each[1],-each[2]});// add ending point
         }
         Collections.sort(height,(a,b) -> {
             if (a[0] != b[0]){
-                return a[0] - b[0];
+                return a[0] - b[0]; // firstly sort by starting point(ascending)
             }else{
-                return b[1] - a[1];
+                return b[1] - a[1]; // if same, then sort by ending point(descending)
             }
         });
-        Queue<Integer> pq = new PriorityQueue<>((a,b) -> (b - a));
+        Queue<Integer> pq = new PriorityQueue<>((a,b) -> (b - a));// max heap
         pq.offer(0);
         int prev = 0;
         for (int[] each : height){
             if (each[1] > 0){
-                pq.offer(each[1]);
+                pq.offer(each[1]);  // offer starting height
             }else{
-                pq.remove(-each[1]);
+                pq.remove(-each[1]);    // remove ending height
             }
             int curr = pq.peek();
             if (prev != curr){
