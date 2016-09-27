3a0027ed798a384fc696f627550be8a51c6c9bfd
dlsyj <dlsyj@users.noreply.github.com>
2016-09-27 13:04:05 -0400

    [26. Remove Duplicates from Sorted Array][Time Limit Exceeded] First Round:

--- 26-Remove-Duplicates-from-Sorted-Array/solution.java
+++ 26-Remove-Duplicates-from-Sorted-Array/solution.java
@@ -4,9 +4,12 @@ public class Solution {
             return 0;
         }
         int size = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] != nums[size]){
-                nums[++size] = nums[i];
+        for (int i = 1; i < nums.length; i++){
+            // if (nums[i] != nums[size]){
+            //     nums[++size] = nums[i];
+            // }
+            if (nums[i] != nums[i-1]){
+                nums[size++] = nums[i];
             }
         }
         return size + 1;    // return length (index + 1)
