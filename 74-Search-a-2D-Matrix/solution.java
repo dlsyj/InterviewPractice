0a515888df3032e017ab68eccf13395427e5c48e
dlsyj <dlsyj@users.noreply.github.com>
2016-10-02 16:13:13 -0700

    [74. Search a 2D Matrix][Accepted] First Round:

--- 74-Search-a-2D-Matrix/solution.java
+++ 74-Search-a-2D-Matrix/solution.java
@@ -1,20 +1,21 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length ==0){
-            return false;
-        }
-        // matrix is m*n
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int low = 0, high = m*n-1;
-        while (low != high){
-            int mid = low + (high - low)/2;
-            if (matrix[mid/n][mid%n] < target){
-                low = mid+1;
-            } else {
-                high = mid;
+        if (matrix == null || matrix.length == 0) {
+                return false;
             }
-        }
-        return matrix[low/n][low%n] == target;
+            int start = 0, rows = matrix.length, cols = matrix[0].length;
+            int end = rows * cols - 1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (matrix[mid / cols][mid % cols] == target) {
+                    return true;
+                } 
+                if (matrix[mid / cols][mid % cols] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            return false;
     }
 }
