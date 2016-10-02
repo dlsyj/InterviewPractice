402e7fbe6bb3f442048541581de08cb34a72028b
dlsyj <dlsyj@users.noreply.github.com>
2016-10-02 11:05:14 -0700

    [380. Insert Delete GetRandom O(1)][Compile Error] First Round:

---
+++ 380-Insert-Delete-GetRandom-O(1)/solution.java
@@ -0,0 +1,45 @@
+public class RandomizedSet {
+    HashMap<Integer, Integer> map;
+    List<Integer> list;
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<Integer, Integer>();
+        list = new ArrayList<Integer>();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (map.containsKey(val)){
+            return false;
+        }
+        map.put(val,map.size() - 1);
+        list.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)){
+            return false;
+        }
+        int prevIndex = map.get(val);
+        int lastElement = list[list.size() - 1];
+        map.remove(val);
+        map.put(lastElement, prevIndex);
+        list.remove(list.size() - 1);
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
