59c28bdb57dacfb7e9d0472fea0d7e65b30de120
dlsyj <dlsyj@users.noreply.github.com>
2016-10-02 09:42:00 -0700

    [89. Gray Code][Accepted] First Round:

--- 89-Gray-Code/solution.java
+++ 89-Gray-Code/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> result = new ArrayList<Integer>();
-        for (int i = 0; i < Math.pow(2,n); i ++){
+        for (int i = 0; i < 1 << n; i ++){
             result.add(i^(i>>1));
         }
         return result;
