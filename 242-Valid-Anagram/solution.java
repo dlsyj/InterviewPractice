de34204e32db245f6e9d490a9f6ab80fd81b4879
dlsyj <dlsyj@users.noreply.github.com>
2016-10-02 10:24:05 -0700

    [242. Valid Anagram][Accepted] First Round:

---
+++ 242-Valid-Anagram/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[256];
+        if (s.length() != t.length()){
+            return false;
+        }
+        for (int i = 0; i < s.length(); i ++){
+            arr[(int)s.charAt(i)] ++;
+        }
+        for (int j = 0; j < t.length(); j ++){
+            if (arr[(int)t.charAt(j)] <= 0){
+                return false;
+            }else{
+                arr[(int)t.charAt(j)]--;
+            }
+        }
+        return true;
+    }
+}
