66171f04b745e45e42753039bfb2d5a75ac15667
dlsyj <dlsyj@users.noreply.github.com>
2016-10-02 18:24:42 -0700

    [387. First Unique Character in a String][Compile Error] First Round:

--- 387-First-Unique-Character-in-a-String/solution.java
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -1,16 +1,21 @@
 public class Solution {
     public int firstUniqChar(String s) {
-        Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
-        Set<Character> set = new HashSet<Character>();
-        for (int i = 0; i < s.length(); i++) {
+        if (s == null || s.length == 0){
+            return -1;
+        }
+        int[] freq = new int[26];
+        int[] loc = new int[26];
+        for (int i = 0; i < s.length; i ++){
             char curr = s.charAt(i);
-            if (set.contains(curr)) {
-                map.remove(curr);
-            } else {
-                map.put(curr, i);
-                set.add(curr);
+            freq[curr - 'a'] ++;
+            loc[curr - 'a'] = i;
+        }
+        int result = -1;
+        for (int i = 0; i < 26; i ++){
+            if (freq[i] == 1){
+                result = result == -1? loc[i] : Math.min(loc[i], result);
             }
         }
-        return map.size() == 0 ? -1 : map.entrySet().iterator().next().getValue();
+        return result;
     }
 }
