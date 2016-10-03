7d6b3d35507b12cead584b55ae298dde9fbac3de
dlsyj <dlsyj@users.noreply.github.com>
2016-10-02 18:24:54 -0700

    [387. First Unique Character in a String][Compile Error] First Round:

--- 387-First-Unique-Character-in-a-String/solution.java
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int firstUniqChar(String s) {
-        if (s == null || s.length == 0){
+        if (s == null || s.length() == 0){
             return -1;
         }
         int[] freq = new int[26];
