84edb829c136a725fb7a4f1686575d715c50f7a5
dlsyj <dlsyj@users.noreply.github.com>
2016-10-02 19:24:34 -0700

    [396. Rotate Function][Compile Error] First Round:

--- 396-Rotate-Function/solution.java
+++ 396-Rotate-Function/solution.java
@@ -6,11 +6,12 @@ public class Solution {
         int oneSum = 0, iteration = 0, len = nums.length;
         for (int i = 0; i < len; i ++){
             oneSum += nums[i];
-            iteration += nums[i] * i
+            iteration += nums[i] * i;
         }
         int result = iteration;
         for (int i = 1; i < len; i ++){
-            result = Math.max(result, iteration - oneSum + len * nums[i - 1]);
+            iteration = iteration - oneSum + len * nums[i - 1];
+            resut = Math.max(result,iteration);
         }
         return result;
     }
