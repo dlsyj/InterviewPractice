369d2af2df44dd629ab75b816836d53fb933f02f
dlsyj <dlsyj@users.noreply.github.com>
2016-10-05 15:36:40 -0400

    [127. Word Ladder][Accepted] First Round:

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-        if (beginWord == null || beginWord.length() == 0 || endWord == null|| endWord.length() == 0 || beginWord.length() != endWord.length(){
+        if (beginWord == null || beginWord.length() == 0 || endWord == null|| endWord.length() == 0 || beginWord.length() != endWord.length()){
             return -1;
         }
         Queue<String> queue = new LinkedList<String>();
         Set<String> set = new HashSet<String>();
-        int result = 0;
-        int currLevNum = 0;
+        int result = 1;
+        int currLevNum = 1;
         int nextLevNum = 0;
         queue.offer(beginWord);
         set.add(beginWord);
@@ -15,7 +15,7 @@ public class Solution {
             String curr = queue.poll();
             char[] currArr = curr.toCharArray();
             for (int i = 0; i < curr.length(); i ++){
-                for (char ch = 'a'; ch < 'z'; ch ++){
+                for (char ch = 'a'; ch <= 'z'; ch ++){
                     currArr[i] = ch;
                     String temp = new String(currArr);
                     if (temp.equals(endWord)){
@@ -35,6 +35,6 @@ public class Solution {
                 result ++;
             }
         }
-        return result;
+        return 0;
     }
 }
