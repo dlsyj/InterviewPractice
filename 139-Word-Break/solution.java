5a23c04403e1701d490d5b344269ef6b177f3ffd
dlsyj <dlsyj@users.noreply.github.com>
2016-10-05 16:07:08 -0400

    [139. Word Break][Compile Error] First Round:

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,25 +1,15 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> dict) {
-        // DFS
-        Set<Integer> set = new HashSet<Integer>();
-        return dfs(s, 0, dict, set);
-    }
-    
-    private boolean dfs(String s, int index, Set<String> dict, Set<Integer> set){
-        // base case
-        if(index == s.length()) return true;
-        // check memory
-        if(set.contains(index)) return false;
-        // recursion
-        for(int i = index+1;i <= s.length();i++){
-            String t = s.substring(index, i);
-            if(dict.contains(t))
-                if(dfs(s, i, dict, set))
-                    return true;
-                else
-                    set.add(i);
+        boolean[] canBreak = new boolean[s.length() + 1];
+        canBreak[0] = true;
+        for (int i = 1; i <= s.length(); i ++){
+            for (int j = 0; j < i; j ++){
+                if (dp[j] && dict.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    continue;
+                }
+            }
         }
-        set.add(index);
-        return false;
+        return dp[s.length() + 1];
     }
 }
