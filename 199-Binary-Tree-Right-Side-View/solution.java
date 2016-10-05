2a58c24876076ffefee602ef1bba1bdffb9d3748
dlsyj <dlsyj@users.noreply.github.com>
2016-10-05 18:19:53 -0400

    [199. Binary Tree Right Side View][Compile Error] First Round:

---
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new List<Integer>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        if (root == null){
+            return result;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int currLevNum = queue.size();
+            for (int i = 0; i < currLevNum; i ++){
+                TreeNode curr = queue.poll();
+                if (i == currLevNum - 1){
+                    result.add(curr.val);
+                }
+                if (curr.left != null){
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null){
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        return result;
+    }
+}
