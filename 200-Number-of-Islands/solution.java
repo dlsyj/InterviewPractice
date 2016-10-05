4058df4a63bf8eb68004834325c7594eb074f860
dlsyj <dlsyj@users.noreply.github.com>
2016-10-05 18:26:14 -0400

    [200. Number of Islands][Runtime Error] First Round:

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -3,29 +3,27 @@ public class Solution {
         if (grid == null || grid.length == 0 || grid[0].length == 0){
             return 0;
         }
-        int numOfIslands = 0;
+        int result = 0;
         for (int i = 0; i < grid.length; i ++){
             for (int j = 0; j < grid[0].length; j ++){
-                if (grid[i][j] != '1'){
-                    continue;
+                if (grid[i][j] == '1'){
+                    dfsHelper(grid, i, j);
+                    result ++;
                 }
-                numOfIslands ++;
-                dfs(grid,i,j);
             }
         }
-        return numOfIslands;
+        return result;
     }
     
-    public void dfs(char[][] grid, int i, int j){
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length){
+    public void dfsHelper(char[][] grid, int i, int j){
+        if (i < 0 || i > grid.length || j < 0 || j > grid[0].length || grid[i][j] != '1'){
             return;
         }
-        if (grid[i][j] == '1'){
-            grid[i][j] = '2';
-            dfs(grid,i-1,j);
-            dfs(grid,i+1,j);
-            dfs(grid,i,j-1);
-            dfs(grid,i,j+1);
-        }
+        grid[i][j] = '*';
+        dfsHelper(grid, i - 1, j);
+        dfsHelper(grid, i + 1, j);
+        dfsHelper(grid, i, j - 1);
+        dfsHelper(grid, i, j + 1);
+        // grid[i][j] = '1';
     }
 }
