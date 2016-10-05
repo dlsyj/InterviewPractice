6be79a6919d020966cadf9909ac5b8a75130d5a4
dlsyj <dlsyj@users.noreply.github.com>
2016-10-05 09:00:49 -0400

    [78. Subsets][Compile Error] First Round:

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,17 +1,16 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-	    List<List<Integer>> list = new ArrayList<>();
-	    Arrays.sort(nums);
-	    backtrack(list, new ArrayList<>(), nums, 0);
-	    return list;
+	    List<List<Integer>> result = new ArrayList<Integer>();
+	    helper(result, nums, new ArrayList<Integer>(), 0);
+	    return result;
 	}
-
-	private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-	    list.add(new ArrayList<Integer>(tempList));
-	    for(int i = start; i < nums.length; i++){
-	        tempList.add(nums[i]);
-	        backtrack(list, tempList, nums, i + 1);
-	        tempList.remove(tempList.size() - 1);
+	
+	public void helper(List<List<Integer>> result, int[] nums, ArrayList<Integer> path, int index){
+	    result.add(new ArrayList<Integer>(path));
+	    for (int i = index; i < nums.length; i ++){
+	        path.add(nums[i]);
+	        helper(result,nums,path,i + 1);
+	        path.remove(path.size() - 1);
 	    }
 	}
 }
