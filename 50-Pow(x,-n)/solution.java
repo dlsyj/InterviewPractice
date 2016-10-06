d5a0d071676f6ca15b69f725a02f6eef87906ed0
dlsyj <dlsyj@users.noreply.github.com>
2016-10-06 11:30:50 -0400

    [50. Pow(x, n)][Accepted] First Round:

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -2,13 +2,15 @@ public class Solution {
     public double myPow(double x, int n) {
         double result = 1;
         long i = Math.abs((long)n);
-        while(i > 0) {
-            if(i % 2 == 1){
+        while (i != 0){
+            if (i % 2 == 1){
                 result *= x;
             }
             i /= 2;
             x *= x;
         }
-        return n < 0 ?  1/result : result;
+        return n > 0 ? result : 1/result;
     }
+    
+    
 }
