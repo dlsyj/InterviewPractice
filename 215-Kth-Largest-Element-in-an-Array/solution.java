e4660f3a882f4a327883ad3b96d3af5b31de5186
dlsyj <dlsyj@users.noreply.github.com>
2016-10-05 22:34:26 -0400

    [215. Kth Largest Element in an Array][Compile Error] First Round:

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,12 +1,36 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        final PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        for (int num : nums){
-            pq.offer(num);
-            if (pq.size() > k){
-                pq.poll();
+        if (nums == null || nums.length < k){
+            throw new Error();
+        }
+        int pos = quickselect(nums, nums.length - k + 1, 0, nums.length - 1);
+        return nums[pos];
+    }
+    public int quickselect(int[] nums, int k, int left, int right){
+        int i = left, j = right;
+        int pivot = nums[right];
+        while (i < j){
+            if (pivot < nums[i]){
+                j --;
+                swap(nums, i, j);
+            }else{
+                i ++;
             }
         }
-        return pq.peek();
+        swap(nums, i, right);
+        int currNum = i - left + 1;
+        if (currNum < k){
+            quickselect(nums, k - currNum, i + 1, right);
+        }else if (k < currNum){
+            quickselect(nums, k, 0, i - 1);
+        }else{
+            return i;
+        }
+    }
+    
+    public void swap(int[] nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
