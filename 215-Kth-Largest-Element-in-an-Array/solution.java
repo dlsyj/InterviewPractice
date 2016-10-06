ef170378b66b3ce695c712fee4d18164aa795252
dlsyj <dlsyj@users.noreply.github.com>
2016-10-05 22:44:39 -0400

    [215. Kth Largest Element in an Array][Runtime Error] First Round:

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -3,10 +3,10 @@ public class Solution {
         if (nums == null || nums.length < k){
             throw new Error();
         }
-        int pos = quickselect(nums, nums.length - k + 1, 0, nums.length - 1);
+        int pos = quickSelect(nums, nums.length - k + 1, 0, nums.length);
         return nums[pos];
     }
-    public int quickselect(int[] nums, int k, int left, int right){
+    public int quickSelect(int[] nums, int k, int left, int right){
         int i = left, j = right;
         int pivot = nums[right];
         while (i < j){
@@ -18,19 +18,19 @@ public class Solution {
             }
         }
         swap(nums, i, right);
-        int currNum = i - left + 1;
-        if (currNum < k){
-            return quickselect(nums, k - currNum, i + 1, right);
-        }else if (k < currNum){
-            return quickselect(nums, k, left, i - 1);
+        int lessPivotNum = i - left + 1;
+        if (lessPivotNum < k){
+            return quickSelect(nums, k - lessPivotNum, i + 1, right);
+        }else if (k < lessPivotNum){
+            return quickSelect(nums, k, left, i - 1);
         }else{
             return i;
         }
     }
     
-    public void swap(int[] nums, int left, int right){
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
